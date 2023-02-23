9060571f434ae34efed85f72f931173a27cbf5e4
nscuro <nscuro@protonmail.com>
2023-02-23 20:23:25 +0100

    Add `Jackson` utility class; Add FPF regression test
    Signed-off-by: nscuro <nscuro@protonmail.com>

---
+++ src/main/java/org/dependencytrack/common/Jackson.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.dependencytrack.common;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Helper class wrapping a Jackson {@link ObjectMapper} and providing various utility methods.
+ *
+ * @since 4.8.0
+ */
+public final class Jackson {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private Jackson() {
+    }
+
+    public static ObjectReader objectReader() {
+        return OBJECT_MAPPER.reader();
+    }
+
+    public static ObjectWriter objectWriter() {
+        return OBJECT_MAPPER.writer();
+    }
+
+    public static JsonNodeFactory nodeFactory() {
+        return OBJECT_MAPPER.getNodeFactory();
+    }
+
+    public static ArrayNode newArray() {
+        return nodeFactory().arrayNode();
+    }
+
+    public static ObjectNode newObject() {
+        return nodeFactory().objectNode();
+    }
+
+    public static JsonNode readString(final String string) throws IOException {
+        return readString(string, JsonNode.class);
+    }
+
+    public static <T> T readString(final String string, Class<T> clazz) throws IOException {
+        return objectReader().readValue(string, clazz);
+    }
+
+    public static JsonNode readHttpResponse(final CloseableHttpResponse response) throws IOException {
+        return readHttpResponse(response, JsonNode.class);
+    }
+
+    public static <T> T readHttpResponse(final CloseableHttpResponse response, final Class<T> clazz) throws IOException {
+        try (final InputStream entityInputStream = response.getEntity().getContent()) {
+            return objectReader().readValue(entityInputStream, clazz);
+        }
+    }
+
+    public static ArrayNode asArray(final JsonNode jsonNode, final String fieldName) {
+        final JsonNode arrayNode = jsonNode.get(fieldName);
+        if (!arrayNode.isArray()) {
+            return nodeFactory().arrayNode();
+        }
+
+        return (ArrayNode) arrayNode;
+    }
+
+}
